bb61e2c1191a725001980e5f30066d932f7cf97a
canezareskat-cell <canezareskat@gmail.com>
2026-03-10 11:00:20 +0700

    penambahan nama nezar

---
+++ Assets/Scripts/menambahkannamanezar.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class menambahkannamanezar : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("nama saya adalah canezares keandre totiro");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
