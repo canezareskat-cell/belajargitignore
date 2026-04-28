25a57e3fbc82570b1112a00b33bd4151280e3050
canezareskat-cell <canezareskat@gmail.com>
2026-04-28 19:02:15 +0700

    connect player to scriptable object (Canezares Keandre Arkana Totiro / 10 pplg1/09

--- Assets/Scripts/Core/GameManager.cs
+++ Assets/Scripts/Core/GameManager.cs
@@ -13,26 +13,47 @@ public class GameManager : MonoBehaviour
 
     void Start()
     {
-        currentState = GameState.Playing;
+        SetState(GameState.Playing);
     }
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
-            PauseGame();
+            if (currentState == GameState.Playing)
+            {
+                PauseGame();
+            }
+            else if (currentState == GameState.Paused)
+            {
+                ResumeGame();
+            }
         }
+    }
+
+    public void SetState(GameState newState)
+    {
+        currentState = newState;
     }
 
     public void PauseGame()
     {
         Time.timeScale = 0f;
-        currentState = GameState.Paused;
+        SetState(GameState.Paused);
+        Debug.Log("Paused");
+    }
+
+    public void ResumeGame()
+    {
+        Time.timeScale = 1f;
+        SetState(GameState.Playing);
+        Debug.Log("Resume");
     }
 
     public void GameOver()
     {
+        Time.timeScale = 0f;
+        SetState(GameState.GameOver);
         Debug.Log("Game Over");
-        currentState = GameState.GameOver;
     }
 }
